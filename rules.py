cf5fa347e7e8b1b3b6394069e8cf3f8704fc156f
neha  singh <your-email@example.com>
2026-05-23 22:48:30 +0530

    Added some files and rule code

---
+++ rules.py
@@ -0,0 +1,16 @@
+FILE_RULES = {
+    ".png": "Images",
+    ".jpg": "Images",
+    ".jpeg": "Images",
+
+    ".pdf": "Documents",
+    ".docx": "Documents",
+    ".txt": "Documents",
+
+    ".mp3": "Music",
+
+    ".zip": "Archives",
+
+    ".py": "Code",
+}
+
